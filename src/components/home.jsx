7c37557191e671e0756d9f2fdd3610b9cbfe8432
Shourya <shouryasinha001@gmail.com>
2021-06-05 21:34:49 +0530

    fixed errors

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,13 +7,13 @@ export const Home = () => {
         <>
     <div style= {{position : "absolute",top : "30%",left : "20%"}}>
         <h2 className= "my-4"><font face="fira code" size="6"color = "white" className = "mx-2 my-4 px-2" >
-        Hello, my name is <span class ="span"> <span className = "letter">S</span><span className = "letter">h</span><span className = "letter">o</span><span className = "letter" >u</span><span className = "letter">r</span><span className = "letter">y</span><span className = "letter">a</span></span></font></h2>
-        <p><font face="fira code" size="4" color = "white" className = "mx-2 my-4 px-2" >I'm a Student, a <span class ="spansub">Pythonista</span>, a <span class ="spansub">Networking guy</span> and looking forward to learn <span class ="spansub">React native</span></font></p>
+        Hello, my name is <span className ="span"> <span className = "letter">S</span><span className = "letter">h</span><span className = "letter">o</span><span className = "letter" >u</span><span className = "letter">r</span><span className = "letter">y</span><span className = "letter">a</span></span></font></h2>
+        <p><font face="fira code" size="4" color = "white" className = "mx-2 my-4 px-2" >I'm a Student, a <span className ="spansub">Pythonista</span>, a <span className ="spansub">Networking guy</span> and looking forward to learn <span className ="spansub">React native</span></font></p>
         <p><font face="fira code" size="4" color = "white" className = "mx-2 my-4 px-2" >On the internet, I'm known as Copyright(C), quite weird huh?</font></p>
         <button type="button" className="mybutton btn btn-info my-3" style = {{width : "120px",}}>View</button> 
         <br/>
         <a href="https://github.com/SATAN01" target="_blank">
-        <button class= "mx-2 my-4"style = {{borderRadius : "50%",height : "58px",width:"58px",backgroundColor : "white"}}><img src={img}/></button>
+        <button className= "mx-2 my-4"style = {{borderRadius : "50%",height : "58px",width:"58px",backgroundColor : "white"}}><img src={img}/></button>
         </a>  
         <a href="https://stackoverflow.com/users/13845215/copyrightc" target="_blank"> 
         <button style = {{borderRadius : "50%",height : "58px",width:"58px",backgroundColor : "white"}}><img src={img2}/></button>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,7 +6,7 @@ export const Projects = () => {
     return (
     
         <div style =  {{position : "absolute", top : "100%", left : "20%"}}>
-            <h1 className ="font">My projects!</h1>
+            <h1 className ="font"><span className = "letter">M</span><span className = "letter">y</span><span className = "letter"> p</span><span className = "letter">r</span><span className = "letter">o</span><span className = "letter">j</span><span className = "letter">e</span><span className = "letter">c</span><span className = "letter">t</span><span className = "letter">s</span><span className = "letter">!</span></h1>
             <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -16,24 +16,13 @@ export const Projects = () => {
   <div className="carousel-inner">
     <div className="carousel-item active">
       <img src={pyc} className="" alt="..."></img>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
     </div>
     <div className="carousel-item">
       <img src={pyc2} className="d-block w-100" alt="..."></img>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+
     </div>
     <div className="carousel-item">
       <img src={react1} className="d-block w-100" alt="..."></img>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -45,7 +34,44 @@ export const Projects = () => {
     <span className="visually-hidden">Next</span>
   </button>
 </div>
+
+        <p id="text">So, I have been programming for a while now, which ulimately led me to develop some cool projects. Here's a list of my top projects : </p> 
+        <hr class = "hr"/>
+        <p className="listitem">
+          
+        1.<a href="https://github.com/SATAN01/GUI-chat-app" target= "_blank"><font className = "my-3 mx-3">Pychat</font> </a>
+          <br/>
+          <br />
+          As the name suggests, Pychat is a GUI chatting app made in python, mainly with the help of tkinter and the sockets module.
+          It also has a <a href="https://github.com/SATAN01/TCP-server.py-client.py">CLI predecessor.</a>
+          Pychat also is one of my first networking projects. Though back then one of my best projects, it had a 
+          drawback. The problem was that the app only supported the open chat feature and no one on one chatting.
+          <br />
+          <br />
+          </p>
+          <hr class = "hr"/>
+          <p className="listitem">
+        2. <a href="https://github.com/SATAN01/DogeChat" target = "_blank">Pychat 2.0</a> (Also known as <i>DogeChat</i>)
+        <br />
+        <br />
+          With considering the problems with Pychat, I decided to make Pychat 2, supported by presonal group chatting,
+          one on one chatting, and with a tons of more features like settings page, support for different resoulutions
+          which is generally not provided in most of the <i>tkinter</i> built-in GUI's
+        </p>
+        
+        <hr class = "hr"/>
+        <p className="listitem">
+        3. <a href="https://github.com/SATAN01/Control-mouse-with-your-fingers" target = "_blank"><font className = "my-3 mx-3">Finger cursor</font></a>
+        <br />
+        <br />
+        Ever been in a situation when your mouse battery just dies and you can't do anything with it? 
+        Not sure about you but I've been in that situation for a million times now and so I decieded to make a app that would 
+        help me control my cursor just with movement of my fingers. Well that's what this repository does.
+        You can control the cursor with your index finger and left click by raising your index and the middle.
+        The app internally uses the <i>open cv</i> and <i>mediapipe</i> module to perform the task. 
+        </p>
         </div>
+        
       
     )
 }
